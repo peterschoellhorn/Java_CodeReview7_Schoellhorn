b678f3716b57cf152a76c171e18464d822ea92c1
peterschoellhorn <65154833+peterschoellhorn@users.noreply.github.com>
2021-04-30 21:36:35 +0200

    Update User.java

--- CR7/src/User.java
+++ CR7/src/User.java
@@ -1,6 +1,6 @@
 public class User {
-  private static final String user = "simon";
-  private static final String password = "simon1";
+  private static final String user = ${"DB_USER"};
+  private static final String password = ${"DB_ROOT"};
 
   public static String getUser() {
     return user;
